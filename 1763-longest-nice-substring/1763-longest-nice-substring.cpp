34131bd1d64c8aa9a2573201d4b406b7d5211164
Harshit Agarwal <82012782+harshit-ag14@users.noreply.github.com>
2022-09-28 21:04:50 +0530

    Time: 14 ms (69.74%), Space: 12.3 MB (58.41%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        unordered_set<char> uset;
+        if(s.size()<2)
+            return "";
+        for(int i=0;i<s.size();i++)
+        {
+            uset.insert(s[i]);
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(uset.count(tolower(s[i]))==true && uset.count(toupper(s[i]))==true)
+                continue;
+
+            string prev=longestNiceSubstring(s.substr(0,i));
+            string next=longestNiceSubstring(s.substr(i+1));
+            return prev.size()>=next.size()? prev : next;
+        }
+            
+
+            return s;
+    }
+};
